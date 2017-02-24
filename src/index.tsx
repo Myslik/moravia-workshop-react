8fca0ef936a53e59014924c3f943d88b3b584b74
Premysl Krajcovic <PremyslK@moravia.com>
2017-02-24 14:39:26 +0100

    Add OData adapter

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Grid } from "./grid";
-import { DemoAdapter } from "./demo";
+import { ODataAdapter } from "./odata";
+
+const adapter = new ODataAdapter(
+    "http://services.odata.org/TripPinRESTierService/People",
+    [
+        { key: "UserName", width: 150 },
+        { key: "FirstName", width: 150 },
+        { key: "LastName", width: 150 }
+    ],
+    "UserName"
+);
 
 ReactDOM.render(
-    <Grid adapter={new DemoAdapter()} />,
+    <Grid adapter={adapter} />,
     document.getElementById("example")
 );
